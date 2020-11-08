38e9f780f6b6cb2676e0b95d3ed0f493b0cc6493
Mathis <mathisdesnoes@gmail.com>
2020-11-08 13:23:38 +0100

    problème de chmod

--- script_personnalisation/conf_defaut/conf_lightdm_defaut.sh
+++ script_personnalisation/conf_defaut/conf_lightdm_defaut.sh
@@ -3,6 +3,8 @@
 
 
 echo "*** Réécriture du fichier lightdm-gtk-greeter.conf ***"
+touch /etc/lightdm/lightdm-gtk-greeter.conf
+sudo chmod u+w,g+w,a+w /etc/lightdm/lightdm-gtk-greeter.conf
 if sudo echo -e "[greeter]\nbackground = /usr/share/backgrounds/160218-deux-two_by_Pierre_Cante.jpg\nicon-theme-name = default\nfont-name = Ubuntu 13\nindicators = ~host;~spacer;~session;~a11y;~language;~clock;~power\nshow-language-selector = false\na11y-states = +reader;+keyboard\nposition = -63%,end -45%,center\ndefault-user-image = #" > /etc/lightdm/lightdm-gtk-greeter.conf ; then
 	echo " ==> OK"
 else
